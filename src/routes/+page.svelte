68367d2dbce396126212aaadde76381a5ba60489
JaeHo Song <thruthesky@gmail.com>
2023-05-05 16:54:26 +0800

    merging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,29 @@
 <script>
 	import { loginUser } from '../lib/stores/app.store';
-	import List from './Components/List.svelte'
-	import View from './Components/View.svelte'
-	let act1 = true
-	let act2 = true
+	import List from './Components/List.svelte';
+	import View from './Components/View.svelte';
+	let act1 = true;
+	let act2 = true;
 </script>
 
 {#if act1}
 	<h1>Welcome to CMS</h1>
-	<button on:click={() => act1 = false}>Make Note</button>
+	<button on:click={() => (act1 = false)}>Make Note</button>
 
-{#if $loginUser}
-	<p>Hi, {$loginUser.name}!</p>
-	<p>My Id is {$loginUser.id}</p>
-
-{:else}
-	{#if act2}
-		<List on:click={() => act2 = false}/>
+	{#if $loginUser}
+		<p>Hi, {$loginUser.name}!</p>
 	{:else}
-		<View />
+		<p>Please <a href="/login">login</a></p>
+		<p>Or <a href="/register">register</a> to continue.</p>
 	{/if}
+{:else if act2}
+	<List
+		on:click={() => {
+			console.log('This code work?');
+			act2 = false;
+			act1 = true;
+		}}
+	/>
+{:else}
+	<View />
 {/if}
---
+++ tmp/test5.sql
@@ -0,0 +1,58 @@
+-- MariaDB dump 10.19  Distrib 10.11.2-MariaDB, for osx10.18 (x86_64)
+--
+-- Host: localhost    Database: test5
+-- ------------------------------------------------------
+-- Server version	10.11.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `idx` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  `email` varchar(32) DEFAULT NULL,
+  `gender` varchar(32) DEFAULT NULL,
+  `createdAt` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idx`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES
+(1,2,'pass','keannu','keannu@ku.com','m',1683258013),
+(2,5,'password','ku','test@test.com','m',1683271905);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-05 16:27:29
