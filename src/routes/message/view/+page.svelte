b693cb4797422ffacf24929a319ee3388b3aaa19
JaeHo Song <thruthesky@gmail.com>
2023-05-05 16:14:30 +0800

    doc

--- src/routes/message/view/+page.svelte
+++ src/routes/message/view/+page.svelte
@@ -1,9 +1,16 @@
 <script>
-
-    export let data;
-    console.log('from view', data)
-    console.log('from view:', data.code)
+	export let data;
+	console.log('from view', data);
+	console.log('from view:', data.code);
 </script>
+
 <h1>Message View</h1>
 
-<div><p>asdf</p><button >Edit</button><button>delete</button></div>
+<div>
+	<p>asdf</p>
+	<button>Edit</button><button>delete</button>
+</div>
+
+<div>sender name, title, date</div>
+
+.
